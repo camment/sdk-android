ad83438d7367686ae8f54204e776e6420a61070d
Petra Adamuscinova <petra.adamuscinova@gmail.com>
2017-10-05 15:26:04 +0200

    adjusted audio source

--- cammentsdk/src/main/java/tv/camment/cammentsdk/camera/MediaAudioEncoder.java
+++ cammentsdk/src/main/java/tv/camment/cammentsdk/camera/MediaAudioEncoder.java
@@ -75,6 +75,7 @@ final class MediaAudioEncoder extends MediaEncoder {
     }
 
     private static final int[] AUDIO_SOURCES = new int[]{
+            MediaRecorder.AudioSource.VOICE_CALL,
             MediaRecorder.AudioSource.MIC,
             MediaRecorder.AudioSource.DEFAULT,
             MediaRecorder.AudioSource.CAMCORDER,
@@ -153,23 +154,15 @@ final class MediaAudioEncoder extends MediaEncoder {
         }
     }
 
-    private byte[] adjustVolume(byte[] audioSamples, float volume) {
+    private byte[] adjustVolume(byte[] audioSamples, double volume) {
         byte[] array = new byte[audioSamples.length];
         for (int i = 0; i < array.length - 1; i+=2) {
-            // convert byte pair to int
-            short buf1 = audioSamples[i+1];
-            short buf2 = audioSamples[i];
+            short audioSample = (short) (((audioSamples[i+1] & 0xff) << 8) | (audioSamples[i] & 0xff));
 
-            buf1 = (short) ((buf1 & 0xff) << 8);
-            buf2 = (short) (buf2 & 0xff);
+            audioSample = (short) (audioSample * volume);
 
-            short res= (short) (buf1 | buf2);
-            res = (short) (res * volume);
-
-            // convert back
-            array[i] = (byte) res;
-            array[i+1] = (byte) (res >> 8);
-
+            array[i] = (byte) audioSample;
+            array[i+1] = (byte) (audioSample >> 8);
         }
         return array;
     }

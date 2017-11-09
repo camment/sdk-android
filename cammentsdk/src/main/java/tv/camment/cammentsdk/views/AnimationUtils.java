1199bc5b8de0172c6a3d112f3d96eecda5959c4c
Petra Adamuscinova <petra.adamuscinova@gmail.com>
2017-11-09 08:49:58 +0100

    added recording button animation and hidden PullableView implementation

--- cammentsdk/src/main/java/tv/camment/cammentsdk/views/AnimationUtils.java
+++ cammentsdk/src/main/java/tv/camment/cammentsdk/views/AnimationUtils.java
@@ -75,30 +75,6 @@ final class AnimationUtils {
         vRecordIndicator.setAlpha(0.0f);
     }
 
-    static void animateActivateRecordingButton(final ImageButton ibRecord) {
-        ObjectAnimator animatorX = ObjectAnimator.ofFloat(ibRecord, "scaleX", 1.0f, 1.5f);
-        ObjectAnimator animatorY = ObjectAnimator.ofFloat(ibRecord, "scaleY", 1.0f, 1.5f);
-        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(ibRecord, "alpha", 0.5f, 0.9f);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animatorX, animatorY, animatorAlpha);
-        animatorSet.setDuration(100);
-        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
-        animatorSet.start();
-    }
-
-    static void animateDeactivateRecordingButton(final ImageButton ibRecord) {
-        ObjectAnimator animatorX = ObjectAnimator.ofFloat(ibRecord, "scaleX", 1.5f, 1.0f);
-        ObjectAnimator animatorY = ObjectAnimator.ofFloat(ibRecord, "scaleY", 1.5f, 1.0f);
-        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(ibRecord, "alpha", 0.9f, 0.5f);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animatorX, animatorY, animatorAlpha);
-        animatorSet.setDuration(100);
-        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
-        animatorSet.start();
-    }
-
     static void animateTooltip(final BaseTooltipView tooltipView) {
         ObjectAnimator objectAnimator =
                 ObjectAnimator.ofFloat(tooltipView, "translationY", 0, 0, -30, 0, -15, 0, 0);
